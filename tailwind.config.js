5bbc82680e2e99ff7b0cfcc8df7a71e32d307589
Wesley Williams <40679192+Moto42@users.noreply.github.com>
2024-04-22 15:55:46 -0500

    clean up old content

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,9 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'sans': ['"noto sans"', ...defaultTheme.fontFamily.sans],
-        'headers': ['"bakbak one"', ...defaultTheme.fontFamily.sans],
+        // here for ease of adding things to the font-stack
+        'sans': [...defaultTheme.fontFamily.sans],
+        'headers': [...defaultTheme.fontFamily.sans],
       },
     },
   },
